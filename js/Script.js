ead977a4d3ff1a4f8176d235016f31d54f0a640b
sloth-panda <achyutpanda488@gmail.com>
2021-02-15 15:24:38 +0530

    added splash screen

---
+++ js/Script.js
@@ -0,0 +1,7 @@
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    setTimeout(() => {
+        splash.classList.add("invisible")
+    }, 1800)
+});
