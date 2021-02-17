7198eba7ade9774d0d1512c5f7f746f82ccc05fa
Amit Yadav <yamit5869@protonmail.com>
2021-02-17 11:38:41 +0530

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -6,3 +6,34 @@ document.addEventListener('DOMContentLoaded', (e) => {
     }, 1800)
 });
 
+const empty = "";
+const lcase = "abcdefghijklmnopqrstuvwxyz";
+
+const len = document.getElementById("numberinput");
+const lowcase = document.getElementById("lowercase");
+const submit = document.getElementById("button");
+const pass = document.getElementById("password");
+
+submit.addEventListener('click', () => {
+
+    let inipass = empty;
+    (lowcase.checked) ? inipass += lcase : "";
+
+    pass.value = generatePassword(len.value, inipass)
+});
+
+function  generatePassword(l, inipass) {
+    let pas = "";
+    for(let i = 0; i < l; i++){
+        pas += inipass.charAt(Math.floor(Math.random() * inipass.length));
+    }
+    return pas;
+}
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    setTimeout(function(){
+        popup.classList.remove("show");
+      },1000);
+  }
