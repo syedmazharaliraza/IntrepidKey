776701d2754c78cd796c7a0b1b632ffc6d9c130c
unknown <yamit5869@protonmail.com>
2021-02-21 23:21:48 +0530

    Revert "Merge branch 'main' of https://github.com/syedmazharaliraza/IntrepidKey"
    This reverts commit 22d2fc4f7678515eae043aee63b08ad0047c2034, reversing
    changes made to 7e19bd89c745813a23af35d104d1f8c62a9c2f9d.

--- js/Script.js
+++ js/Script.js
@@ -7,22 +7,19 @@ document.addEventListener('DOMContentLoaded', (e) => {
     }, 1800)
 });
 
-// Script for generating symbols and small alphabets password
+// Script for generating only small alphabets password
 const empty = "";
 const lcase = "abcdefghijklmnopqrstuvwxyz";
-const sym = "!@#$%^&*(){}[],.;";
 
 const len = document.getElementById("numberinput");
 const lowcase = document.getElementById("lowercase");
 const submit = document.getElementById("button");
 const pass = document.getElementById("password");
-const symbols = document.getElementById("symbol");
 
 submit.addEventListener('click', () => {
 
     let inipass = empty;
     (lowcase.checked) ? inipass += lcase : "";
-    (symbols.checked) ? inipass += sym : "";
 
     pass.value = generatePassword(len.value, inipass)
 });
